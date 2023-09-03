1549b100c77f4b160891c4225d32ac490702e6b1
Paula Domingues <pdomingues@itba.edu.ar>
2023-09-03 15:41:53 -0300

    fix: style

--- ej1.py
+++ ej1.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
-from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import sys
+import os
 
 
 def read_genbank_file(filename):
@@ -9,16 +9,17 @@ def read_genbank_file(filename):
         records = SeqIO.parse(file, "genbank")
         return list(records)
 
-def translate_mrna_to_aminoacids(mrna_sequence, min_seq_len = 90, start_codon="ATG"):
+
+def translate_mrna_to_aminoacids(mrna_sequence, min_seq_len=90, start_codon="ATG"):
     translations = []
 
-    for i in range(len(mrna_sequence)-2):
-        if mrna_sequence[i]+mrna_sequence[i+1]+mrna_sequence[i+2] == start_codon:
+    for i in range(len(mrna_sequence) - 2):
+        if mrna_sequence[i] + mrna_sequence[i + 1] + mrna_sequence[i + 2] == start_codon:
             max_seq_length = len(mrna_sequence) - i
             max_seq_length_rounded = max_seq_length - (max_seq_length % 3)
-            translation = mrna_sequence[i:max_seq_length_rounded+i].translate(to_stop=True)
-            if len(translation) > min_seq_len:   
-                translations.append((len(translation), translation ,i))
+            translation = mrna_sequence[i:max_seq_length_rounded + i].translate(to_stop=True)
+            if len(translation) > min_seq_len:
+                translations.append((len(translation), translation, i))
 
     translations.sort(reverse=True)
     return translations
@@ -30,50 +31,59 @@ def write_fasta_file(output_filename, translation, record_data):
         SeqIO.write(record, file, "fasta")
 
 
-def translate_genbank_to_fasta(genbank_records, output_filename):
+def translate_genbank_to_fasta(records):
     translations = {}
-    for record in genbank_records:
+    for record in records:
         translations[record.id] = translate_mrna_to_aminoacids(record.seq)
     return translations
 
 
-if __name__ == '__main__':
+def main():
     if len(sys.argv) != 2:
         print("Wrong arguments amount")
         exit(1)
 
     gbk_filename = sys.argv[1]
 
-    line_length = 70
-
     if gbk_filename.split(".")[1] != "gbk" and gbk_filename.split(".")[1] != "gb":
         print("Wrong format for input or output file")
         exit(1)
 
+    try:
+        os.mkdir('./FASTA')
+    except FileExistsError:
+        print("./FASTA directory already exists. Skipping...")
+    except OSError as error:
+        print("Error in creating directory : " + str(error))
+
     faa_filename = "./FASTA/" + gbk_filename.split(".")[0].split("/")[-1] + ".fasta"
     print(faa_filename)
 
-    orfs = []
+    genbank_records = []
 
     try:
         genbank_records = read_genbank_file(gbk_filename)
-    except Exception:
+    except IOError:
         print("Could not open " + gbk_filename)
 
-    translations = translate_genbank_to_fasta(genbank_records, faa_filename)
+    translations = translate_genbank_to_fasta(genbank_records)
 
     for record in genbank_records:
         print(">>> RECORD " + record.id)
         print("-----------------")
         for translation in translations[record.id]:
-            print("sequence: ", translation[1], "length: " , translation[0], " start position: ", translation[2])
+            print("sequence: ", translation[1], "length: ", translation[0], " start position: ", translation[2])
             print("-----------------")
 
         try:
             write_fasta_file(faa_filename, translations[record.id][0][1], record)
-        except Exception:
+        except IOError:
             print("Could not open " + gbk_filename)
 
         print(
             f"Las secuencias de aminoácidos del {record.name} ({record.description}) "
             f"se han guardado en {faa_filename}.")
+
+
+if __name__ == '__main__':
+    main()
