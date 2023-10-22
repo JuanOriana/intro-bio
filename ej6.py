513f633ef66f42acc66d7b2a21ea0d8880415a47
Paula Domingues <pdomingues@itba.edu.ar>
2023-10-22 16:51:08 -0300

    fix: get summary of protein associated with gene

--- ej6.py
+++ ej6.py
@@ -13,40 +13,47 @@ def get_ncbi_description(accession: str):
         seq_record = SeqIO.read(io.StringIO(record), "genbank")
         description = seq_record.description
 
-        return description
+        comments = seq_record.annotations.get("comment", "")
+        summary = ""
+
+        for paragraph in comments.split('.\n'):
+            if paragraph.startswith("Summary:"):
+                summary = paragraph.strip("Summary: ").strip()
+                break
+
+        return description, summary
     except Exception as e:
         print(f"Error fetching information for {accession}: {e}")
+        return None, None
 
 
 def get_proteins_descriptions(df: pd.DataFrame):
-    names = []
-    accessions = []
-    descriptions = []
-
     unique_values = set(
-        df[df['Official Symbol Interactor A'] != 'CNGA3']['Official Symbol Interactor A'].unique()) | set(
+        df[df['Official Symbol Interactor A'] != gene_name]['Official Symbol Interactor A'].unique()) | set(
         df['Official Symbol Interactor B'].unique())
     print("Distinct proteins:", len(unique_values))
 
+    proteins_info = []
+
     for value in unique_values:
-        sub_df_a = df[(df['Official Symbol Interactor A'] == value) & (df['Official Symbol Interactor A'] != 'CNGA3')]
+        sub_df_a = df[(df['Official Symbol Interactor A'] == value) & (df['Official Symbol Interactor A'] != gene_name)]
         sub_df_b = df[(df['Official Symbol Interactor B'] == value)]
 
         if not sub_df_a.empty:
-            accession = sub_df_a.iloc[0]['REFSEQ Accessions Interactor A'].split('|')[0]
-            description = get_ncbi_description(accession)
+            accession = sub_df_a.iloc[0]['REFSEQ Accessions Interactor A'].split('|')
         elif not sub_df_b.empty:
-            accession = sub_df_b.iloc[0]['REFSEQ Accessions Interactor B'].split('|')[0]
-            description = get_ncbi_description(accession)
+            accession = sub_df_b.iloc[0]['REFSEQ Accessions Interactor B'].split('|')
         else:
             accession = ""
-            description = ""
 
-        names.append(value)
-        accessions.append(accession)
-        descriptions.append(description)
+        for unique_accession in accession:
+            description, summary = get_ncbi_description(unique_accession)
+            if summary:
+                break
 
-    new_df = pd.DataFrame({'name': names, 'accession': accessions, 'description': descriptions})
+        proteins_info.append({'name': value, 'description': description, 'summary': summary})
+
+    new_df = pd.DataFrame(proteins_info)
 
     return new_df
 
@@ -63,5 +70,6 @@ def main():
 
 
 if __name__ == '__main__':
+    gene_name = 'CNGA3'
     Entrez.email = "pdomingues@itba.edu.ar"
     main()
